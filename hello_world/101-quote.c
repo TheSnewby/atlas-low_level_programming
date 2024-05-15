dfb0d53bc348ee1b7bcb8fc27b4395dd01efc49c
TheSnewby <stephen.j.newby@gmail.com>
2024-05-15 19:23:04 +0000

    sizeof change

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -11,6 +11,6 @@
 int main(void)
 {
 char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, message, sizeof(char));
+write(2, message, sizeof(message));
 return (1);
 }
