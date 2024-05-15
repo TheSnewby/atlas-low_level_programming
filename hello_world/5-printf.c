042ee097e0aefb2107bac86743947313befdcd91
TheSnewby <stephen.j.newby@gmail.com>
2024-05-15 17:10:20 +0000

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a simple phrase using the main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+        printf("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
