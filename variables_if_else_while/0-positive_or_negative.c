39b40699d0a0ece83d5d7532d1d4994b386528e3
TheSnewby <stephen.j.newby@gmail.com>
2024-05-15 22:14:37 +0000

    added return character

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is posiitve\n", n);
+		printf("%d is positive\n", n);
 	} else if (n == 0)
 	{
 		printf("%d is zero\n", n);
