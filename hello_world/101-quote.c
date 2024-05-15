c6a0e81fc9372d91b274f6189b7d6d1ce7de0147
TheSnewby <stephen.j.newby@gmail.com>
2024-05-15 19:32:28 +0000

    bunch a stuff

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -11,6 +11,7 @@
 int main(void)
 {
 char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-write(2, message, sizeof(message));
+write(2, message, (sizeof(message) - 1));
+write(2, "\n", 1);
 return (1);
 }
