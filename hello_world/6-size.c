b51af67b9643336670c7b6157fa01477b29146dc
TheSnewby <stephen.j.newby@gmail.com>
2024-05-15 17:41:54 +0000

    betty

---
+++ hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the size of data types using the sizeof command.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
