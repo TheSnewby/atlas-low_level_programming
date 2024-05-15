5c0210ec114273ea910eb7f426647a47beadd908
TheSnewby <stephen.j.newby@gmail.com>
2024-05-15 16:51:38 +0000

    betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
-/* Prints a simple phrase using the puts function */
-
-int main(void)
+int main(void) /* Prints a simple phrase using the puts function */
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
