3bc0d48eb497da958efd71737366b3a870a2747f
TheSnewby <stephen.j.newby@gmail.com>
2024-05-16 14:49:08 +0000

    for betty

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,17 +9,17 @@
 int main(void)
 {
 	char letter;
-	
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		putchar(letter);
 	}
-	
+
 	for (letter = 'A'; letter <= 'Z'; letter++)
 	{
 		putchar(letter);
 	}
 	putchar('\n');
-	
+
 	return (0);
 }
