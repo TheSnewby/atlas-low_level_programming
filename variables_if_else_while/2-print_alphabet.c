03722d98a3833e785f2794fb52fa495fe32bfa20
TheSnewby <stephen.j.newby@gmail.com>
2024-05-16 14:37:42 +0000

    betty

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet using putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+	for (letter= 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
