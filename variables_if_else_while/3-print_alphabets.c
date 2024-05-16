1432ea4be897e89451177b64c6f06da26cde7fe2
TheSnewby <stephen.j.newby@gmail.com>
2024-05-16 14:48:13 +0000

    for putchar

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,18 +8,18 @@
 
 int main(void)
 {
-        char letter;
-
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-                putchar(letter);
-        }
-
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	
 	for (letter = 'A'; letter <= 'Z'; letter++)
 	{
 		putchar(letter);
 	}
-        putchar('\n');
-
-        return (0);
+	putchar('\n');
+	
+	return (0);
 }
