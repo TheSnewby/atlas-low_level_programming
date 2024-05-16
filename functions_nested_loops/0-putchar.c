db2cf7b19e8811ffe6a846a1d444d6a9bda942b2
TheSnewby <stephen.j.newby@gmail.com>
2024-05-16 23:59:19 +0000

    iterating through a string

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
  * main - prints "_putchar \n" without libraries
@@ -8,7 +8,15 @@
 
 int main(void)
 {
-	char str[] = "_putchar\n";
-	write(1, str, sizeof(str));
+	char str[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+
 	return (0);
 }
