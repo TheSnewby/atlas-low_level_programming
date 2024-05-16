df60ac05b28032c1f7e876bf849bf3f276561cea
TheSnewby <stephen.j.newby@gmail.com>
2024-05-16 15:10:44 +0000

    '0'+int

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints integers to putchar 
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar('0' + num);
+	
+	putchar("\n");
+
+	return 0;
+}
