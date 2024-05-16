d777131677ba5ff23a8316934cc7d4262bae00a4
TheSnewby <stephen.j.newby@gmail.com>
2024-05-16 15:36:58 +0000

    hmm

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,13 +10,16 @@ int main(void)
 {
 	int num;
 
-	for(num = 0; num < 9; num++)
+	for(num = 0; num <= 9; num++)
 	{
 		putchar('0' + num);
-		putchar(',');
-		putchar(' ');
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar('9');
+	putchar('\n');
 
 	return (0);
 }
