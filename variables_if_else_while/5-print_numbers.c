46c31e141e99161a5d48b59549ba0fe475077e63
TheSnewby <stephen.j.newby@gmail.com>
2024-05-16 15:03:04 +0000

    for

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single integers
+ *
+ * return: 0
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		printf("%d", number);
+	}
+
+	printf("\n");
+	
+	return 0;
+}
