35ad8f2052159f94b01eebaa78c3a876287ec39b
TheSnewby <stephen.j.newby@gmail.com>
2024-05-17 15:57:39 +0000

    ascii

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * islower - returns 1 if lowercase character
+ *
+ * Return: 1 if true, 0 if false
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return 1;
+	} else 
+	{
+		return 0;
+	}
+}
