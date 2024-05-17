eb9f6349c64c9a3d64c36c87cfeb8955fa076b55
TheSnewby <stephen.j.newby@gmail.com>
2024-05-17 15:49:39 +0000

    nested fors

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -14,7 +14,9 @@ void print_alphabet_x10(void)
 	for (i = 1; i <= 10; i++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
+		{
 			_putchar(letter);
+			_putchar('\n');
+		}
 	}
-	_putchar('\n');
 }
