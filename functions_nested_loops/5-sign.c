bee2448e1c0184be9ba9db45aa173b1d9611e930
TheSnewby <stephen.j.newby@gmail.com>
2024-05-17 16:36:49 +0000

    if then

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of an int
+ * @n: first param
+ *
+ * Return: 1 if pos, 0 if 0, -1 if neg
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+
+	return (-1);
+}
