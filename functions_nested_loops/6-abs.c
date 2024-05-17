0783ca37497aa761e6b4bb9dda11c4fdba5f2fc5
TheSnewby <stephen.j.newby@gmail.com>
2024-05-17 16:44:02 +0000

    type casting

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include 'main.h'
+
+/**
+ * _abs - returns abs of int
+ * @i: first param
+ *
+ * Return: 0
+ */
+
+int _abs(int i)
+{
+	unsigned int j;
+	
+	j = (unsigned int) i;
+
+	return (j);
+}
