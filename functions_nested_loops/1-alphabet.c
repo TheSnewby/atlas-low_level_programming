b663a93cc9edfd61ef31a33997f29aa8745c20b1
TheSnewby <stephen.j.newby@gmail.com>
2024-05-17 15:47:50 +0000

    betty

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,25 +6,11 @@
  * Return: void
  */
 
-void print_alphabet(void)
-{
-
-        char letter;
-
-        for (letter = 'a'; letter <= 'z'; letter++)
-                _putchar(letter);
-        _putchar('\n');
-}
-
-
-/*int main(void)
+void  print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 	_putchar('\n');
-
-	return (0);
 }
-*/
