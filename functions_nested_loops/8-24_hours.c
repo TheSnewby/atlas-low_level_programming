812270f314e39f2718baa1214753164193b622ad
TheSnewby <stephen.j.newby@gmail.com>
2024-05-18 18:51:24 +0000

    checker finicky change

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -11,13 +11,13 @@ void jack_bauer(void)
 	int a, b, c, d;
 	int control = 0;
 
-	for(a = 0; a < 3 && control == 0 ; a++)
+	for (a = 0; a < 3 && control == 0 ; a++)
 	{
-		for(b = 0; b < 10 && control == 0; b++)
+		for (b = 0; b < 10 && control == 0; b++)
 		{
-			for(c = 0; c < 6 && control == 0; c++)
+			for (c = 0; c < 6 && control == 0; c++)
 			{
-				for(d = 0; d < 10 && control == 0; d++)
+				for (d = 0; d < 10 && control == 0; d++)
 				{
 					_putchar('0' + a);
 					_putchar('0' + b);
