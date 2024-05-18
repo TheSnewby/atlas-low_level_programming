1c4a77ff4fb983013f9733aeaa4cb6e1d635e533
TheSnewby <stephen.j.newby@gmail.com>
2024-05-18 03:28:27 +0000

    started 100-print_comb3.c

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints all unique combo of two digits
+ *
+ * Return: 0
+ */
+
+int isinarray(int value, int array[]);
+int swap(int value);
+
+int main(void)
+{
+	int i;
+
+
+}
+
+
+
+int isinarray(int value; int array[])
+{
+	
+
+}
+
+int swap(int value)
+{
+	
+}
