f17b4afe608addf960bd3721bc17a9f6b0efbdff
TheSnewby <stephen.j.newby@gmail.com>
2024-05-18 03:57:11 +0000

    save

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -16,7 +16,13 @@ int main(void)
 
 }
 
-
+/**
+ * isinarray - checks whether a value is in an array
+ * @value: 1st param
+ * @array: 2nd param
+ *
+ * Return: 0 if true, 1 if false
+ */
 
 int isinarray(int value; int array[])
 {
@@ -24,7 +30,21 @@ int isinarray(int value; int array[])
 
 }
 
+/**
+ * swap - reverses values
+ * @value: 1st param
+ *
+ * Return: reversed values
+ */
+
 int swap(int value)
 {
-	
+	int a, b, ba;
+
+	a = value / 10; /* tens digit */
+	b = value % 10; /* ones digit */
+
+	ba = b * 10 + a; /* reversed value */
+
+	return (ba);
 }
