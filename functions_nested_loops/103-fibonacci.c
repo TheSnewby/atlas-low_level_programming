ef14f212c79555bb9b84ebb2b62e4f09595722c2
TheSnewby <stephen.j.newby@gmail.com>
2024-05-20 01:49:56 +0000

    hmm

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print stuff
+ *
+ * Return: 0
+ */
+
+int main (void)
+{
+	int minustwo = 1;
+	int minusone = 2;
+	int current = 0;
+	int evensum = minusone;
+
+	while (current <= 4000000)
+	{
+		current = minustwo + minusone;
+		minustwo = minusone;
+		minusone = current;
+
+		if (current % 2 == 0)
+			evensum += current;
+	}
+
+	printf("%d\n", evensum);
+}
