eaf8d7aefbdf969f6b4b26d7216d59057440327b
TheSnewby <stephen.j.newby@gmail.com>
2024-05-20 15:45:47 +0000

    logic

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -6,92 +6,54 @@
  * Return: 0
  */
 
-int isinarray(int value, int array[]);
-int swap(int value);
-
 int main(void)
 {
-	int i, emptyindex = 0;
-	int array[500]; /* tracks all values no longer unique */
+	int i, j,  emptyindex, inarray = 0; /* inarray = 0 if not in array */
+	int tens, ones, swap; 
+	int arraylength = 200;
+	int array[arraylength]; /* tracks all values no longer unique */
 
-	for (i = 1; i < 100; i++)
+	for (i = 0; i < 100; i++)
 	{
-		if (isinarray(i, array) == 1) /* i not in array */
+		swap = (i % 10 * 10) + (i / 10);
+		
+		for (j = 0; j <= arraylength; j++)
+		{
+			if (i == array[j] || swap == array[j])
+				inarray = 1;
+		}
+
+		if (inarray == 1)
+			continue;
+		else
 		{
 			if (i < 10)
 			{
-				array[emptyindex] = i;
-				emptyindex++;
-				array[emptyindex] = i * 10;
-				emptyindex++;
-				if (i == 1)
-				{
-					_putchar('0');
-					_putchar('0' + i);
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar('0');
-					_putchar('0' + i);
-				}
+				tens = 0;
+				ones = i;
+				swap = i * 10;
 			} else
 			{
-				array[emptyindex] = i;
-				emptyindex++;
-				array[emptyindex] = swap(i);
-				emptyindex++;
-
-				_putchar(',');
-				_putchar(' ');
-				_putchar('0' + i / 10);
-				_putchar('0' + i % 10);
+				tens = i / 10;
+				ones = i % 10;
 			}
+			
+			array[emptyindex] = i;
+			emptyindex++;
+			array[emptyindex] = swap;
+			emptyindex++;
+
+			if (i != 0)
+			{
+				_putchar(",");
+				_putchar(" ");
+			}
+
+			_putchar('0' + tens);
+			_putchar('0' + ones);
 		}
 
-
-	}
-	return (0);
-}
-
-/**
- * isinarray - checks whether a value is in an array
- * @value: 1st param
- * @array: 2nd param
- *
- * Return: 0 if true, 1 if false
- */
-
-int isinarray(int value; int array[])
-{
-	int n;
-	int swapped = swap(value);
-
-	for(n = 0; n < 500; n++)
-	{
-		if ((value == array[n]) && (swapped == array[n]))
-			return (0);
+		inarray = 0;
 	}
 
-	return (1);
-}
-
-/**
- * swap - reverses values
- * @value: 1st param
- *
- * Return: reversed values
- */
-
-int swap(int value)
-{
-	int a, b, ba;
-
-	a = value / 10; /* tens digit */
-	b = value % 10; /* ones digit */
-
-	ba = b * 10 + a; /* reversed value */
-
-	return (ba);
-}
+	_putchar("\n");
