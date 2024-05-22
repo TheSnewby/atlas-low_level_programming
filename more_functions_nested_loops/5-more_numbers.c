11d9d0690da51165dc9a2168f609843d1905e0a1
TheSnewby <stephen.j.newby@gmail.com>
2024-05-22 15:02:15 +0000

    ascii

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -13,6 +13,10 @@ void more_numbers(void)
 	for (i = 0; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
-			_putchar('0' + j);
+		{
+			if (j > 9)
+				_putchar('0' + (j / 10));
+			_putchar('0' + (j % 10);
 	}
+	_putchar('\n');
 }
