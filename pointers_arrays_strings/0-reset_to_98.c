473b1a7fc41c4d5268f8e488e3fac25069f614c4
TheSnewby <stephen.j.newby@gmail.com>
2024-05-22 16:32:15 +0000

    pointers

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - sets a value to 98
+ * @*n: pointer to an integer
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef main_h
+#define main_h
+#include <unistd.h>
+
+int _putchar(char);
+void reset_to_98(int*);
+void swap_int(int*, int*);
+int _strlen(char*);
+void _puts(char*);
+void print_rev(char*);
+void rev_string(char*);
+void puts2(char*);
+void puts_half(char*);
+void print_array(int*, int);
+char *_strcpy(char*, char*);
+int _atoi(char*);
+
+#endif
