4ac690120b517d11b3476ec9ebebd2e22617eccd
TheSnewby <stephen.j.newby@gmail.com>
2024-05-22 20:26:03 +0000

    pointers

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,10 +11,12 @@ void rev_string(char *s)
 {
 	int i;
 	int length = 0;
-	char *s_copy = s;
+	char *s_copy;
 
 	for (i = 0; s[i] != '\0'; i++)
 		length++;
+	for (i = 0; i < length; i++)
+		s_copy[i] = s[i];
 	for (i = 0; i < length; i++)
 		s[i] = s_copy[length - i - 1];
 }
