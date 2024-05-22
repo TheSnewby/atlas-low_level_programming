fa76dd4456e3cad7ad7d8fcf4b09df734de8d056
TheSnewby <stephen.j.newby@gmail.com>
2024-05-22 15:24:33 +0000

    ascii

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws diagonal lines in the terminal
+ * @n: 1st param
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j <= i; j++)
+			{
+				if (j != i)
+					_putchar(' ');
+				else
+					_putchar(92);
+			}
+			_putchar('\n');
+		}
+	}
+
+	else
+		_putchar('n');
+}
