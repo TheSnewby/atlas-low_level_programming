187b31a1f102e4fd34c2f457877afbec1def9eb0
TheSnewby <stephen.j.newby@gmail.com>
2024-05-22 17:03:24 +0000

    pointers

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+/**
+ * _strlen - returns the length of a string
+ * s: c string?
+ *
+ * Return: int 
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != "\0")
+	{
+		count++;
+		*s++;
+	}
+
+	return (count);
+}
