c766bf5236e57c5cdfe2c9568f6a5206c38b1442
TheSnewby <stephen.j.newby@gmail.com>
2024-05-22 15:29:00 +0000

    ascii

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square followed by a \n
+ * @size: 1st param
+ *
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+			_putchar('#');
+	}
+
+	_putchar('\n');
+}
