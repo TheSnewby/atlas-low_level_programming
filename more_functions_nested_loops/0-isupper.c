d440c50e5b0a2d6b220acbd867adafb5c8d56278
TheSnewby <stephen.j.newby@gmail.com>
2024-05-22 14:32:27 +0000

    ascii

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - checks uppercase of char
+ * @c: 1st param
+ *
+ * Return: 1 if uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
