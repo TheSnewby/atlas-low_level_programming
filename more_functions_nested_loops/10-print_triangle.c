a7377220db4001397316c801c54457e51b076f30
TheSnewby <stephen.j.newby@gmail.com>
2024-05-22 15:47:01 +0000

    ascii

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a right-to-left triangle followed by a new line
+ * @size: 1st param
+ *
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = size; i > 0; i--)
+	{
+		for (j = 1; j < i; i++)
+			_putchar(' ');
+		for (j = i; j <= size; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+
+	if (size <= 0)
+		_putchar('\n');
+}
