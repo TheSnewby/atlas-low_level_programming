52991f77c50c380d9b2096902195a2d86999d4a5
TheSnewby <stephen.j.newby@gmail.com>
2024-05-22 19:23:37 +0000

    pointers

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a reversed string \n
+ * @s: string
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = _strlen(*s); s[i] != *s; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
