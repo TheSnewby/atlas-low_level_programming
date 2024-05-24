0e646cc546aca21c462e945a108d20d997969580
TheSnewby <stephen.j.newby@gmail.com>
2024-05-24 18:30:20 +0000

    changes

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * main - prints largest prime factor of a number
@@ -9,10 +9,10 @@
 
 int main(void)
 {
-	int i,j;
-	int n = 612852475143;
-	int maxprime = 0;
-	int isprime = 1; /* boolean, 0 false, 1 true */
+	long i,j;
+	long n = 612852475143;
+	long maxprime = 0;
+	int isprime = 0; /* boolean, 0 false, 1 true */
 
 	for (i = 2; i < (n / 2 + 1); i++)
 	{
@@ -22,15 +22,15 @@ int main(void)
 		{
 			if (n % j == 0)
 			{
-				isprime = 0;
+				isprime = 1;
 				break;
 			}
 		}
-		if (isprime == 0)
+		if (isprime == 1)
 			maxprime = j;
 		isprime = 1;
 	}
 	if (maxprime == 0)
 		maxprime = n;
-	printf("%d", maxprime);
+	printf("%ld", maxprime);
 }
