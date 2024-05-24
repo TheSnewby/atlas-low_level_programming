5fb918367d5d2e12b124b8a185102216fa968589
TheSnewby <stephen.j.newby@gmail.com>
2024-05-24 16:16:04 +0000

    advanced

---
+++ pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - generates random valid passwords
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+
+	return (0);
+}
