8d573b0b18a571db7d2f58ff53f91b9dd8a01857
TheSnewby <stephen.j.newby@gmail.com>
2024-05-24 15:24:50 +0000

    sign

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -24,11 +24,13 @@ int _atoi(char *s)
 			found++; /* found is true */
 			if (found == 1)
 				num = s[i] - '0';
-			else if ((num == 214748364 && s[i] == 8) || (num == -214748364 && s[i] ==8))
+			else if ((num == 214748364 && s[i] == 8)
 			{
 				if (sign < 0)
 					return (-2147483648);
 			}
+			else if (num == -214748364 && s[i] == 8)
+				return (-2147483648);
 			else
 				num = num * 10 + (s[i] - '0');
 		}
