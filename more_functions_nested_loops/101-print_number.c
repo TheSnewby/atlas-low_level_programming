d71ad42b5adf07dea03ae44f1bee24e2457e2815
TheSnewby <stephen.j.newby@gmail.com>
2024-05-25 17:00:01 +0000

    laugh

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#include <limits.h>
+#include <stdio.h>
 
 /**
  * print_number - prints an integer
@@ -9,13 +10,27 @@
 
 void print_number(int n)
 {
+	if (n == INT_MIN)
+	{
+		_putchar('-');
+		_putchar('2');
+		n = 147483648;
+	}
 	if (n < 0)
 	{
 		_putchar('-');
 		n = n * -1;
 	}
 
-	if ((n / 10) > 0)
+	if (n / 10 > 0)
 		print_number(n / 10);
 	_putchar(n % 10 + '0');
 }
+/*
+int main(void)
+{
+	int x = -55;
+	print_number(x);
+	return (0);
+}
+*/
