e9f0a69682003077bd17cb88cd5a08f4b349e312
TheSnewby <stephen.j.newby@gmail.com>
2024-05-28 01:09:16 +0000

    hm

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -10,31 +10,51 @@
 
 int main(void)
 {
-	long i,j;
-	long n = 612852475143;
+	long i;
+	long n = 30; //612852475143;
+	long n_original = n;
+	/* long n = 31; */
 	long maxprime = 0;
-	int isprime = 0; /* boolean, 0 false, 1 true */
-	long primemax = (long)sqrt(n); /* maximum possible prime */
+	int nisprime = 1; /* boolean, 0 false, 1 true */
+	//long maxpossible; * maximum possible prime 
 
-	if (primemax % 2 != 0) /* reduce by 1 if lower*/
-		primemax--;
-
-	for (i = primemax; i > 2 && isprime == 0; i -= 2) /* from primemax reduce by 2 */
+	while (n % 2 == 0) /* remove multiples of 2 */
 	{
-		if (i % 2 == 0 || i % 3 == 0) /* most likely divisors */
-			continue;
-		printf("current i is: %lu\n", i);
-		for (j = 5; j <= primemax; j += 6) /* all other primes above 3 */
-		{
-			if (n % j == 0 || n % (j + 2) == 0) 
-			{
-				isprime = 1; /* is a prime */
-				break;
-			}
-		}
-		if (isprime == 1)
-			maxprime = j;
+		nisprime = 0;
+		maxprime = 2;
+		n /= 2;
 	}
-	printf("%ld\n", maxprime);
+	while (n % 3 == 0) /* remove multiples of 3 */
+	{
+		nisprime = 0;
+		maxprime = 3;
+		n /= 3;
+	}
+
+	printf("maxprime before for loop is %ld\n", maxprime);
+	printf("n is now: %ld\n", n);
+	for (i = 5; i * i <=  n_; i += 6) /* based on 6k +- 1 */
+	{
+		while (n % i == 0) /* remove multiples of 6k - 1 */
+		{
+			printf("n is divisible by %ld\n", i);
+			nisprime = 0;
+			maxprime = i;
+			n = n / i;
+		}
+		printf("maxprime before n i + 2 loop: %ld\n", maxprime);
+		printf("n is now: %ld\n", n);
+		while (n % (i + 2) == 0) /* remove multiples of 6k + 1 */
+		{
+			printf("n is divisible by %ld\n", i + 2);
+			nisprime = 0;
+			maxprime = i + 2;
+			n = n / (i + 2);
+		}
+	}
+
+	if (nisprime == 1) /* when the original number is its own prime */
+		maxprime = n_original;
+	printf("the maximum prime is %ld\n", maxprime);
 	return (0);
 }
