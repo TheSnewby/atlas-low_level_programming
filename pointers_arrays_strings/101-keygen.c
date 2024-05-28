17c11f7aa1dc9e597feda0ef8bc44a2ee174af24
TheSnewby <stephen.j.newby@gmail.com>
2024-05-28 13:59:41 -0500

    keygen

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -8,13 +8,53 @@
  * Return: 0
  */
 
-int keygen(void)
+char* keygen(int size)
 {
+	char c;
+	int passwordIndex; /* set to negative for default */
+	char *password[size];
+	int usedIndices[size] = {-1};
+	int usedIndex = 0;
 	int i;
 
 	srand(time(0));
-	i = rand() - RAND_MAX / 2;
-	printf("%d", i);
+	/* upper case */
+	passwordIndex = rand() % size;
+	password[passwordIndex] = (char)((rand() % 26) + 65);
+	usedIndices[usedIndex] = passwordIndex;
+	usedIndex++;
 
-	return (i);
+	/* lower case */
+	do {passwordIndex = rand() % size;}
+	while(password[passwordIndex] != -1);
+	password[passwordIndex] = (char)((rand() % 26) + 97);
+	usedIndices[usedIndex] = passwordIndex;
+	usedIndex++;
+
+	/* special character */
+	do {passwordIndex = rand() % size;}
+	while(password[passwordIndex] != -1);
+	password[passwordIndex] = (char)((rand() % 26) + 97);
+	usedIndices[usedIndex] = passwordIndex;
+	usedIndex++;
+
+	/* numbers */
+	for(i = 3; i < size; i++)
+	{
+		do {passwordIndex = rand() % size;}
+		while(password[passwordIndex] != -1);
+		password[passwordIndex] = (char)((rand() % 5) + 33);
+		usedIndices[usedIndex] = passwordIndex;
+		usedIndex++;
+	}
+
+	return (password);
+}
+
+int main(void)
+{
+	int size = 8; /* assume 8 characters */
+	char *password = keygen(size);
+	printf("generated password: %s", *password);
+	return (0);
 }
