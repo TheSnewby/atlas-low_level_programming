f4bb985326911ee19b6505eabeea5b07b6a8c0fb
TheSnewby <stephen.j.newby@gmail.com>
2024-05-29 10:38:29 -0500

    control logic

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,9 +12,15 @@ int _strcmp(char *s1, char *s2)
 {
 	int diff = 0;
 	int i;
+	int s1end = 1;
+	int s2end = 1;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	for (i = 0; s1end != 0 && s2end != 0; i++)
 	{
+		if (s1[i] == '\0')
+			s1end = 0;
+		if (s2[i] == '\0')
+			s2end = 0;
 		diff += s1[i] - s2[i];
 	}
 
