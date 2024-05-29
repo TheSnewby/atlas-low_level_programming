401932be0ab7d35c612e5982941b09949ce45f8f
TheSnewby <stephen.j.newby@gmail.com>
2024-05-29 09:25:10 -0500

    VSCode Setting Changes for Betty Appeasement

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -4,24 +4,27 @@
  * _strcat - concatenates two strings
  * @dest: string being concatenated to
  * @src: string added to end of dest
- * 
+ *
  * Return: dest
 */
 
 char *_strcat(char *dest, char *src)
 {
-    int i = 0;
-    int j;
+	int i = 0;
+	int j;
 
-    while (dest[i] != '\0')
-        i++;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
-    for (j = 0; src[j] != '\0'; j++)
-    {
-        dest[i] = src[j];
-        i++;
-    }
-    dest[i] = '\0';
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
 
-    return (dest);
+	dest[i] = '\0';
+
+	return (dest);
 }
