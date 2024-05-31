ca8deb5b8c9332798e3cc36a982ffd6f2cd3e9d7
TheSnewby <stephen.j.newby@gmail.com>
2024-05-31 16:22:05 -0500

    sum

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prints the sum of the two diags of a square matrix
+ * @a: square matrix
+ * @size: size of matrix
+ *
+ * Return: void
+*/
+
+void print_diagsums(int *a, int size);
+{
+	int i;
+	int sumLtoR = 0;
+	int sumRtoL = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sumLtoR += a[i][i];
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		sumRtoL += a[i][size - i];
+	}
+	printf("%d, %d", sumLtoR, sumRtoL);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 #include <unistd.h>
+#include <stdio.h>
 
 int _putchar(char);
 void reset_to_98(int*);
