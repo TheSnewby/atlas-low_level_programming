96038013d94bf1f87345ba27b1579c425befecfe
TheSnewby <stephen.j.newby@gmail.com>
2024-05-31 13:11:43 -0500

    function

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring in a string
+ * @haystack: string
+ * @needle: substring
+ *
+ * Return: pointer or NULL
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+	int needle_length = 0;
+
+	for (i = 0; needle[i] != '\0'; i++) /* length of needle */
+		needle_length++;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i+j] != needle[j])
+					break;
+				else if (j == needle_length - 1)
+					return (&haystack[i]);
+			}
+		}
+	}
+	return (NULL);
+}
