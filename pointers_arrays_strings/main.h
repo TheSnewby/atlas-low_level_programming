73409eee14c8d06a75b535d3d9dd96f3481bea0b
TheSnewby <stephen.j.newby@gmail.com>
2024-05-31 12:14:13 -0500

    memcopy

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -23,5 +23,13 @@ void reverse_array(int*, int);
 char *string_toupper(char*);
 char *cap_string(char*);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int);
+char *_strchr(char*, char);
+unsigned int _strspn(char*, char*);
+char *_strpbrk(char*,char*);
+char *_strstr(char*, char*);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int*, int);
+
 
 #endif
