35dd803bf16b4883f59af7ce18670a1835337045
TheSnewby <stephen.j.newby@gmail.com>
2024-06-01 17:45:57 -0500

    stupid single array instead of matrix

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <math.h>
 
 /**
  * print_diagsums - prints the sum of the two diags of a square matrix
@@ -13,15 +14,16 @@ void print_diagsums(int *a, int size)
 	int i;
 	int sumLtoR = 0;
 	int sumRtoL = 0;
+	int m = sqrt(size);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < m; i++)
 	{
-		sumLtoR += a[i][i];
+		sumLtoR += a[i * (m + 1)];
 	}
 
-	for (i = 0; i < size; i++)
+	for (i = 1; i <= m; i++)
 	{
-		sumRtoL += a[i][size - i];
+		sumRtoL += a[i * (m - 1)];
 	}
 	printf("%d, %d", sumLtoR, sumRtoL);
 }
