15306309a87ada7abcce79b43008e588af4ff009
TheSnewby <stephen.j.newby@gmail.com>
2024-06-02 20:55:59 -0500

    rotfang conspiracy

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @str: string being encoded
+ *
+ * Return: string pointer
+*/
+
+char *rot13(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		while ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			if ((str[i] >= 'A' && str[i] <= 'M') || (str[i] >= 'a' && str[i] <= 'm'))
+				str[i] += 13;
+			else
+				str[i] -= 13;
+		}
+	}
+	return (str);
+}
