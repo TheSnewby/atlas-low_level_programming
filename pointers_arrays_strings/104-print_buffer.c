16f9305ceb82ee73b8b9460e322502530bc90ab4
TheSnewby <stephen.j.newby@gmail.com>
2024-06-03 23:22:34 -0500

    print_buffer start

---
+++ pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer
+ * @size: number of bytes of buffer
+ *
+ * Return: void
+*/
+
+void print_buffer(char *b, int size)
+{
+    int i, j;
+
+    if (size <= 0) /* 0 size case */
+    	printf("\n");
+    for (i = 0; b[i] != '\0'; i += 10)
+    {
+		printf("%08x: ", (&b[i] - &b[0])); 
+
+    	for (j = 0; j < 10; j += 2)
+    	{
+			printf("%02x%02x ", (unsigned int)(b[i + j]), (unsigned int)(b[i + j + 1]));
+    	}
+    	for (j = 0; j < 10; j++)
+    	{
+			if (b[i + j] > 31 || b[i + j] < 127)
+				printf("%c", b[i + j]);
+			else
+				printf(".");
+    	}
+    printf("\n");
+    }
+}
