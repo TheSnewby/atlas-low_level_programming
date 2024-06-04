379838080f743b3abe463ea73e8c9677bdb47340
TheSnewby <stephen.j.newby@gmail.com>
2024-06-03 23:56:29 -0500

    betty

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -14,22 +14,21 @@ void print_buffer(char *b, int size)
 
 	if (size <= 0) /* 0 size case */
 		printf("\n");
-		for (i = 0; i < size; i += 10)
-		{
-			printf("%08x: ", (unsigned int)(&b[i] - &b[0]));
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", (unsigned int)(&b[i] - &b[0]));
 
-			for (j = 0; j < 10; j += 2)
-			{
-				printf("%02x%02x ", (unsigned int)(b[i + j]),
-				(unsigned int)(b[i + j + 1]));
-			}
-			for (j = 0; j < 10; j++)
-			{
-				if ((b[i + j]) > 31 && (b[i + j]) < 127)
-					printf("%c", b[i + j]);
-				else
-					printf(".");
-			}
-			printf("\n");
+		for (j = 0; j < 10; j += 2)
+		{
+			printf("%02x%02x ", (unsigned int)(b[i + j]), (unsigned int)(b[i + j + 1]));
 		}
+		for (j = 0; j < 10; j++)
+		{
+			if ((b[i + j]) > 31 && (b[i + j]) < 127)
+				printf("%c", b[i + j]);
+			else
+				printf(".");
+		}
+		printf("\n");
+	}
 }
