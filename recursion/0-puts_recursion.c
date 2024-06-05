a80d71ae148c3451b977038760b7ad9f19495647
TheSnewby <stephen.j.newby@gmail.com>
2024-06-05 09:55:57 -0500

    removed main from testing

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -10,15 +10,9 @@ void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 		_putchar('\n');
-	else 
+	else
 	{
 		_putchar(*s);
 		s++;
 	}
-}
-
-int main(void)
-{
-    _puts_recursion("Puts with recursion");
-    return (0);
 }
