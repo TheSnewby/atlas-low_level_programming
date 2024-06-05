c1ade57df5adfcde987d6269d90d636a7801d73f
TheSnewby <stephen.j.newby@gmail.com>
2024-06-05 11:37:11 -0500

    removed static var

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -9,10 +9,7 @@
 
 int _strlen_recursion(char *s)
 {
-	static int sum = 1; /* betty doesn't want 0 */
-
 	if (*s == '\0')
-		return (sum - 1);
-	sum++;
-	return(_strlen_recursion(s + 1));
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
