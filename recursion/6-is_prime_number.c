e00ce66040040d977404b99b3d2c10bd49e0cb4a
TheSnewby <stephen.j.newby@gmail.com>
2024-06-05 21:57:25 -0500

    backup

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * is_prime_number - prime number indicator
+ * @n: number
+ *
+ * Return: 1 if prime, otherwise 0 if false
+*/
+
+int is_prime_number(int n)
+{
+	if (base case)
+		return 
+}
