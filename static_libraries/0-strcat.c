5f06990a916778c8842e5cbe87aa11fde5be7407
TheSnewby <stephen.j.newby@gmail.com>
2024-06-06 16:33:51 -0500

    update

---
+++ static_libraries/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: string being concatenated to
+ * @src: string added to end of dest
+ *
+ * Return: dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
