e229408efaae3de634e93acb360e49de452c090f
TheSnewby <stephen.j.newby@gmail.com>
2024-06-07 13:40:29 -0500

    segfaulting

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -13,24 +13,25 @@ int main(int argc, char **argv)
 	int i;
 	int sum = 0;
 
-	if (argc < 1)
+	if (argc < 2)
 	{
-		printf("Error\n");
-		return (1);
+		printf("0\n");
 	}
 
-	for (i = 1; *argv[i] != '\0'; i++)
+	else
 	{
-		if (argv[i][0] < '0' || argv[i][0] > '9')
+		for (i = 1; *argv[i] != '\0'; i++)
 		{
-		printf("Error\n");
-		return (1);
+			if (argv[i][0] < '0' || argv[i][0] > '9')
+			{
+			printf("Error\n");
+			return (1);
+			}
+			sum += atoi(argv[i]);
 		}
 
-		sum += atoi(argv[i]);
+		printf("%d\n", sum);
 	}
-
-	printf("%d\n", sum);
 
 	return (EXIT_SUCCESS);
 }
