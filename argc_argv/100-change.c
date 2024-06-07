d0c2fa68ad3224150f0271dc1959779516718787
TheSnewby <stephen.j.newby@gmail.com>
2024-06-07 15:47:13 -0500

    silly logic

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -11,38 +11,39 @@
 int main(int argc, char *argv[])
 {
 	int coin_count = 0;
+	int cents;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	
-	int cents = atoi(argv[1]);
+	else
+		cents = atoi(argv[1]);
 
 	if (cents < 0)
 		printf("0\n");
 	else
 	{
-		while (cents / 25 >= 0)
+		while (cents >= 25)
 		{
 			coin_count += 1;
 			cents -= 25;
 		}
 
-		while (cents / 10 >= 0)
+		while (cents >= 10)
 		{
 			coin_count += 1;
 			cents -= 10;
 		}
 
-		while (cents / 5 >= 0)
+		while (cents >= 5)
 		{
 			coin_count += 1;
 			cents -= 5;
 		}
 
-		while (cents / 1 >= 0)
+		while (cents >= 1)
 		{
 			coin_count += 1;
 			cents -= 1;
