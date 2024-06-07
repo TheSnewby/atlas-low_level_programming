5e9683389f8d55961bf9bf5a1299258efbe872e1
TheSnewby <stephen.j.newby@gmail.com>
2024-06-07 10:43:16 -0500

    first helper function

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,15 +1,30 @@
 #include "main.h"
 
 /**
+ * _sqrt_recursion_helper - helper function to find a natural sqrt
+ * @i: natural numbers to check if sqrt
+ * @value: original value to compared against
+ *
+ * Return: -1 if no natural sqrt, otherwise the value
+ */
+
+int _sqrt_recursion_helper(int i, int value)
+{
+	if (i * i == value)
+		return (i);
+	else if (i > value / 2)
+		return (-1);
+	_sqrt_recursion_helper(i + 1, value);
+}
+
+/**
  * _sqrt_recursion - recursively find the natural sqrt of a number
  * @n: number
  *
- * Return: int
+ * Return: -1 if no natural sqrt, otherwise the value
 */
 
 int _sqrt_recursion(int n)
 {
-	if (n == 0)
-		return (-1);
-	return (n * _sqrt_recursion(n - 1));
+	return (_sqrt_recursion_helper(1, n));
 }
