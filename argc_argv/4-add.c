c0c447f5a3fde0a26208b17037c5c1721a9b922c
TheSnewby <stephen.j.newby@gmail.com>
2024-06-07 13:08:59 -0500

    first attempt

---
+++ argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main - prints the addition of positive numbers
+ * @argc: number of arguments
+ * @argv: string of strings
+ *
+ * Return: EXIT_SUCCESS
+*/
+
+int main(int argc, char **argv)
+{
+	int i;
+	int sum = 0;
+
+	if (argc < 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (i = 1; argv[i] != '\0'; i++)
+	{
+		if (argv[i][0] < '0' || argv[i][0] > '9')
+		{
+		printf("Error\n");
+		return (1);
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (EXIT_SUCCESS);
+}
