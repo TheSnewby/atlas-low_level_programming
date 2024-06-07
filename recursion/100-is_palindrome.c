59088df03231dd210a70174eb2ced1e462240a78
TheSnewby <stephen.j.newby@gmail.com>
2024-06-07 17:48:49 -0500

    removed main

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -46,23 +46,3 @@ int is_palindrome(char *s)
 
 	return(is_palindrome_helper(s, size));
 }
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int r;
-
-    r = is_palindrome("level");
-    printf("%d\n", r);
-    r = is_palindrome("redder");
-    printf("%d\n", r);
-    r = is_palindrome("test");
-    printf("%d\n", r);
-    r = is_palindrome("step on no pets");
-    printf("%d\n", r);
-    return (0);
-}
