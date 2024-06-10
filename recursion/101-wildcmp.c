48c9b9fcc3b67e0d19227c17b52084e2eb8a5e20
TheSnewby <stephen.j.newby@gmail.com>
2024-06-10 16:55:10 -0500

    some updates

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -10,10 +10,12 @@
 int wildcmp_helper(char *s1, char *s2, int i)
 {
   if (*s1 == '\0' && *s2 == '\0')
-  	return (0);
+  	return (i);
   else if (*s1 == '\0')
   	return (-1);
-  if (*s1 == *s2)
+  if (*s2 == '*')
+	return (wildcmp_helper(s1, s2 + 1, i));
+  else if (*s1 == *s2)
 	return(i);
   else
   {
