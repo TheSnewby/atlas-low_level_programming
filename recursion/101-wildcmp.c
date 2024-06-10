15af550d08cb3f93ee7c30f6dccd534cdd4d2569
TheSnewby <stephen.j.newby@gmail.com>
2024-06-10 15:00:33 -0500

    asterisk math

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -14,7 +14,9 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 	else if (*s1 == '\0' || *s2 == '\0')
 		return (0);
-	if (*s1 == *s2)
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1));
+	else if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	else
 		return (0);
