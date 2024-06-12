fdc6bf932b86e79dcae095088c0c9849d0d92bd4
TheSnewby <stephen.j.newby@gmail.com>
2024-06-12 12:11:23 -0500

    saving progress

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * alloc_grid - creates and returns a 2 dim array of ints
+ * @width: width
+ * @height: height
+ *
+ * Return: double pointer, otherwise NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
+	
+}
