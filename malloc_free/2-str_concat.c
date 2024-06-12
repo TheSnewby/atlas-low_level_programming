74b12f0f00a21ea4c2aeb884dc3328e9f72e46c8
TheSnewby <stephen.j.newby@gmail.com>
2024-06-12 11:46:01 -0500

    silly syntax

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,7 +10,9 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int size_s1, size_s2, i = 0;
+	int size_s1 = 0;
+	int size_s2 = 0;\
+	int i = 0;
 	char *str = NULL;
 
 	if (s1 == NULL || s2 == NULL)
@@ -23,8 +25,8 @@ char *str_concat(char *s1, char *s2)
 	str = malloc(size_s1 + size_s2 + 1);
 	for (i = 0; i < size_s1; i++)
 		str[i] = s1[i];
-	for (; i < size_s1 + size_s2; i++)
-		str[i] = s2[i];
+	for (i; i < size_s1 + size_s2; i++)
+		str[i] = s2[i - size_s1];
 	if (str == NULL)
 		return (NULL);
 	str[size_s1 + size_s2] = '\n';
