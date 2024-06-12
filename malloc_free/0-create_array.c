65ab77f4da79249e6cc4114c2ef98ce7f279f469
TheSnewby <stephen.j.newby@gmail.com>
2024-06-12 10:29:06 -0500

    order

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,11 +10,13 @@
 
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i = 0;
+	char *array = NULL;
+
 	if (size == 0)
 		return (NULL);
 
-	unsigned int i = 0;
-	char *array = malloc(sizeof(char) * size + 1);
+	array = malloc(sizeof(char) * size + 1);
 
 	if (array == NULL)
 		return (NULL);
