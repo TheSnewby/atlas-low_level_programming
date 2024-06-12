8082f09dbb14f9618385d4c69a67920313d74778
TheSnewby <stephen.j.newby@gmail.com>
2024-06-12 11:57:28 -0500

    null treatment

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,13 +15,16 @@ char *str_concat(char *s1, char *s2)
 	int i = 0;
 	char *str = NULL;
 
-	/*  if (s1 == NULL || s2 == NULL)
-		return (NULL); */
-
-	for (i = 0; s1[i] != '\0'; i++)
-		size_s1++;
-	for (i = 0; s2[i] != '\0'; i++)
-		size_s2++;
+	if (s1 == NULL) 
+		size_s1 = 0;
+	else
+		for (i = 0; s1[i] != '\0'; i++)
+			size_s1++;
+	if (s2 == NULL)
+		size_s2 = 0;
+	else
+		for (i = 0; s2[i] != '\0'; i++)
+			size_s2++;
 	str = malloc(size_s1 + size_s2 + 1);
 	for (i = 0; i < size_s1; i++)
 		str[i] = s1[i];
