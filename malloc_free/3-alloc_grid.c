0679ed486f2ea27cd90d121d7988db37115f8441
TheSnewby <stephen.j.newby@gmail.com>
2024-06-12 13:52:27 -0500

    row x column instead of column x line

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -21,9 +21,9 @@ int **alloc_grid(int width, int height)
 	if (grid == NULL)
 		return (NULL);
 
-	for (i = 0; i < height; i++)
+	for (i = 0; i < width; i++)
 	{
-		for (j = 0; j < width; j++)
+		for (j = 0; j < height; j++)
 		{
 			grid[i][j] = 0;
 		}
