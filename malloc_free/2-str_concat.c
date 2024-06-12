4a2c8a4f3b14e047a6717f8b9260e8d204279c55
TheSnewby <stephen.j.newby@gmail.com>
2024-06-12 11:32:03 -0500

    first time?!

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * str_concat - concatenates two strings to new space
+ * @s1: string being concatenated to
+ * @s2: string being concatenated
+ *
+ * Return: pointer to new space
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int size_s1, size_s2, i = 0;
+	char *str = NULL;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		size_s1++;
+	for (i = 0; s2[i] != '\0'; i++)
+		size_s2++;
+	str = malloc(size_s1 + size_s2 + 1);
+	for (i = 0; i < size_s1; i++)
+		str[i] = s1[i];
+	for (; i < size_s1 + size_s2; i++)
+		str[i] = s2[i];
+	if (str == NULL)
+		return (NULL);
+	str[size_s1 + size_s2] = '\n';
+	return (str);
+}
