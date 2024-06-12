3e8f56cd827d8b7681e1a57916a083a04d6b0bbe
TheSnewby <stephen.j.newby@gmail.com>
2024-06-12 13:39:32 -0500

    first 2D

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -10,5 +10,23 @@
 
 int **alloc_grid(int width, int height)
 {
-	
+	int i, j;
+	int **grid = NULL;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(sizeof(int) * width * height);
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
 }
