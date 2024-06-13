f04d1669a23f3f719f196ff03363d7c1b375ef08
TheSnewby <stephen.j.newby@gmail.com>
2024-06-13 16:36:38 -0500

    returning to first ternary with small adjustment

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,8 +1,5 @@
 #ifndef function_like_macro_h
 #define function_like_macro_h
-#define ABS(x) ({ \
-	int X = (x); \
-	(X) < (0) ? (-X) : (X); \
-	})
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
 #endif
