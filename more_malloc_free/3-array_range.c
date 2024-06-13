311bbfa807b285aceb74d67ec3ff1d9ce794fef1
TheSnewby <stephen.j.newby@gmail.com>
2024-06-13 15:00:43 -0500

    reverting

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -16,11 +16,11 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	arr = malloc(sizeof(int) * (max - min) + 1);
+	arr = malloc(sizeof(int) * (max - min));
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i <= (max - min); i++)
+	for (i = 0; i < (max - min); i++)
 		arr[i] = min + i;
 
 	return (arr);
