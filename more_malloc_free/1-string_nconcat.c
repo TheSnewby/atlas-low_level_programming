36b120f3aeac0843270cb99f99cbd7598b2b9f82
TheSnewby <stephen.j.newby@gmail.com>
2024-06-13 12:36:48 -0500

    no effect

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 
 	for (i = 0; i < s1_size; i++)
-		s[i] == s1[i];
+		s[i] = s1[i];
 	for (j = 0; j < s2_size || j < n; j++)
 		s[s1_size + j] = s2[j];
 	s[s1_size + j] = '\0';
