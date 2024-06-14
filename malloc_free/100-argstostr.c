f7a0b0d4038c5a9be6931ac933429eaa4ce878ce
TheSnewby <stephen.j.newby@gmail.com>
2024-06-14 11:06:52 -0500

    argtostr

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * argstostr - concatenates all arguments of program
+ * @ac: assuming argc
+ * @av: assuming argv
+ *
+ * Return: pointer or NULL if fails
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j;
+	char *str = NULL;
+	int size = 0;
+	int k = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 1; i <= ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			size++;
+		}
+	}
+
+	str = malloc(sizeof(char) * (size + ac - 1));
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 1; i <= ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+
+	str[k] = '\0';
+	return (str);
+}
