f0c6eca7e34ee9697fa76e906ffab47488fc6709
TheSnewby <stephen.j.newby@gmail.com>
2024-06-14 12:01:36 -0500

    bettsy

--- malloc_free/100-argstostr.c
+++ malloc_free/100-argstostr.c
@@ -45,17 +45,3 @@ char *argstostr(int ac, char **av)
 	str[k] = '\0';
 	return (str);
 }
-/*
-int main(int ac, char *av[])
-{
-    char *s;
-
-    s = argstostr(ac, av);
-    if (s == NULL)
-    {
-        return (1);
-    }
-    printf("%s", s);
-    free(s);
-    return (0);
-} */
