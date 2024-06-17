499bd031eb16360724817088be25bf17d984c38b
TheSnewby <stephen.j.newby@gmail.com>
2024-06-16 21:32:29 -0500

    betty

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -45,18 +45,3 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	return (s);
 }
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- 
-int main(void)
-{
-    char *concat;
-
-    concat = string_nconcat("Best ", "School !!!", 6);
-    printf("%s\n", concat);
-    free(concat);
-    return (0);
-} */
