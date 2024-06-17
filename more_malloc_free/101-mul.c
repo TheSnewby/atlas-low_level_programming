0bfc9020480b423b3bce8825b02730ce12d0e11a
TheSnewby <stephen.j.newby@gmail.com>
2024-06-16 22:01:09 -0500

    101-mul.c first

---
+++ more_malloc_free/101-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * mul - multiplies two numbers passed as arguments
+ * num1 - num of arguments
+ * num2 - ** of arguments
+ *
+ * Return: 0 if succesful, 98 otherwise
+ */
+
+int mul(int num1, int **num2)
+{
+	if (num1 != 2)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	printf("%d\n", num[0] * num[1]);
+	return (0);
+}
