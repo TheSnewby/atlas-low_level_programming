b972064594ff0d9f61d4e97e8164eb9ee8afbd71
TheSnewby <stephen.j.newby@gmail.com>
2024-06-18 16:02:36 -0500

    1-init

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include "main.h"
+
+/**
+ * init_dog - initializing a dog class
+ * @d: struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	d->age = age;
+	d->owner = owner;
+}
