8d30bbb13f3e54364ec7a94680ee021714077b7d
TheSnewby <stephen.j.newby@gmail.com>
2024-06-20 14:06:32 -0500

    freed dogs

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * free_dog - frees allocated memory of a dog struct
+ * @d: dog struct
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	int i;
+
+	free((*d).name);
+	free((*d).owner);
+	free(d);
+}
