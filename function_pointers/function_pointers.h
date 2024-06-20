06a05dcd0c608a689318067b3bbd15a6d6de386f
TheSnewby <stephen.j.newby@gmail.com>
2024-06-20 14:50:22 -0500

    removed commenting from header

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,10 +1,9 @@
 #ifndef function_pointers_h
 #define function_pointers_h
 
+void _putchar(char);
 void print_name(char *, void (*)(char *));
-/*
 void array_iterator(int *, size_t, void (*)(int));
 int int_index(int *, int, int (*)(int));
-*/
 
 #endif
