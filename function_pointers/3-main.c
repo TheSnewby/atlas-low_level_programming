95031784beca61c3cef4ee5409f7277c279cda0d
TheSnewby <stephen.j.newby@gmail.com>
2024-06-20 23:38:27 -0500

    added debug msg

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
 	argv[2][0] == '*' || argv[2][0] == '/' || argv[2][0] == '%'))
 	{
 		output = get_op_func(argv[2])(num1, num2);
-		printf("%d\n", output);
+		printf("%d paradiddle\n", output);
 		return (0);
 	}
 	else
