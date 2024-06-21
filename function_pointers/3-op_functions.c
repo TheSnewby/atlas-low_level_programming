e29f3e641e5912af0408887a7f0e715fc2840d13
TheSnewby <stephen.j.newby@gmail.com>
2024-06-20 23:44:23 -0500

    changed returns to exits in op_functions and added printfs

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -43,7 +43,10 @@ int op_mul(int a, int b)
 int op_div(int a, int b)
 {
 	if (b == 0)
-		return (100);
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 /**
@@ -56,6 +59,9 @@ int op_div(int a, int b)
 int op_mod(int a, int b)
 {
 	if (b == 0)
-		return (100);
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
