57c23abf3b56c131b22ee6c1ee466a813d2a76b6
TheSnewby <stephen.j.newby@gmail.com>
2024-06-22 00:35:25 -0500

    changed logic and added null check

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -11,39 +11,43 @@ void print_all(const char * const format, ...)
 	int i = 0;
 	va_list text;
 	char *str;
+	int va_count = 0;
 
 	va_start(text, format);
-	while (format[i] != '\0')
+	while (format[i] != '\0' && format != NULL)
 	{
-		if ((i > 0) && (format[i] == 'c' || format[i] == 'i'
+		if ((va_count > 0) && (format[i] == 'c' || format[i] == 'i'
 		|| format[i] == 'f' || format[i] == 's'))
 			printf(", ");
 		switch (format[i])
 		{
 			case 'c':
-			printf("%c", va_arg(text, int));
-			break;
+				printf("%c", va_arg(text, int));
+				va_count++;
+				break;
 			case 'i':
-			printf("%d", va_arg(text, int));
-			break;
+				printf("%d", va_arg(text, int));
+				va_count++;
+				break;
 			case 'f':
-			printf("%f", va_arg(text, double));
-			break;
+				printf("%f", va_arg(text, double));
+				va_count++;
+				break;
 			case 's':
-			str = va_arg(text, char*);
-			if (str == NULL)
-			{
-				printf("(nil)");
-				continue;
-			}
-			printf("%s", str);
-			break;
+				str = va_arg(text, char*);
+				va_count++;
+				if (str == NULL)
+				{
+					printf("(nil)");
+					continue;
+				}
+				printf("%s", str);
+				break;
 			default:
-			break;
+				break;
 		}
 		i++;
 	}
 	va_end(text);
 	printf("\n");
 }
-
