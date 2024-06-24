cfcceb69c6de9f298f6e13df2a63a8b44321d7e5
TheSnewby <stephen.j.newby@gmail.com>
2024-06-24 14:06:11 -0500

    betty

--- function_pointers/100-main_opcodes.c
+++ function_pointers/100-main_opcodes.c
@@ -25,9 +25,9 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		return (2);
 	}
-
-	str = (unsigned char *)main; /* converts main code to a string in its compiled form? */
-	for(i = 0; i < n; i++)
+	/* converts main code to a string in its compiled form */
+	str = (unsigned char *)main;
+	for (i = 0; i < n; i++)
 	{
 		if (i != 0)
 			printf(" ");
