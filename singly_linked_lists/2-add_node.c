e9a357c7b51d5a01b61fd3cd8a6413bcabec2ba6
TheSnewby <stephen.j.newby@gmail.com>
2024-07-02 11:12:18 -0500

    addnode

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node a thte beginning of a list_t list
+ * @head: head of list_t list
+ * @str: str to be added
+ *
+ * Return: address of new element, or NULL if failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = NULL;
+	char *str_copy = NULL;
+
+	new_node = (list_t *)malloc(sizeof(list_t));
+	str_copy = strdup(str);
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = str_copy;
+	new_node->next = *head;
+	(*head) = new_node;
+
+	return (&new_node);
+}
