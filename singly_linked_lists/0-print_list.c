d025d22fa6071e28df2641e783ccc43f113515ff
TheSnewby <stephen.j.newby@gmail.com>
2024-07-02 10:47:22 -0500

    0-print_list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: head of list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while(h != NULL)
+	{
+		if (h->str == NULL)
+			printf("%s\n", "[0] (nil)");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
