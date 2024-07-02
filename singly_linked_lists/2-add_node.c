d93b18dddd5a2505e56727009172c4b627e7f011
TheSnewby <stephen.j.newby@gmail.com>
2024-07-02 11:27:45 -0500

    added len

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,6 +12,8 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = NULL;
 	char *str_copy = NULL;
+	unsigned int length = 0;
+	int i = 0;
 
 	new_node = (list_t *)malloc(sizeof(list_t));
 	str_copy = strdup(str);
@@ -19,8 +21,14 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 
+	while (str[i])
+	{
+		i++;
+	}
+
 	new_node->str = str_copy;
 	new_node->next = *head;
+	new_node->len = i;
 	(*head) = new_node;
 
 	return (*head);
