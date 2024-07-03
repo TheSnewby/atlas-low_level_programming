91110b53cb6bb295c11ce300e1524bf30d8dfcc5
TheSnewby <stephen.j.newby@gmail.com>
2024-07-03 13:38:30 -0500

    forgot to intialize temp

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp = NULL;
+	listint_t *temp = *head;
 	unsigned int count = 0;
 	listint_t *new_node = NULL;
 
