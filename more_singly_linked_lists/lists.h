1c8b15351efc24aaa4e0bbf86cc6ca977d82718d
TheSnewby <stephen.j.newby@gmail.com>
2024-07-03 08:25:07 -0500

    betty for header

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -9,24 +9,23 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
-size_t print_listint(const listint_t*);
-size_t listint_len(const listint_t*);
-listint_t *add_nodeint(listint_t **head, const int n);
-listint_t *add_nodeint_end(listint_t **head, const int);
-void free_listint(listint_t *head);
-void free_listint2(listint_t **head);
-int pop_listint(listint_t **head);
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-int sum_listint(listint_t *head);
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-int delete_nodeint_at_index(listint_t **head, unsigned int index);
+size_t print_listint(const listint_t *);
+size_t listint_len(const listint_t *);
+listint_t *add_nodeint(listint_t **, const int);
+listint_t *add_nodeint_end(listint_t **, const int);
+void free_listint(listint_t *);
+void free_listint2(listint_t **);
+int pop_listint(listint_t **);
+listint_t *get_nodeint_at_index(listint_t *, unsigned int);
+int sum_listint(listint_t *);
+listint_t *insert_nodeint_at_index(listint_t **, unsigned int, int);
+int delete_nodeint_at_index(listint_t **, unsigned int);
 
 #endif
