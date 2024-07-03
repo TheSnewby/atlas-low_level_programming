63a641891537a79da7192fd2928a975cf2870efa
TheSnewby <stephen.j.newby@gmail.com>
2024-07-03 07:54:04 -0500

    changed while to includ != NULL

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -10,35 +10,10 @@ size_t list_len(const list_t *h)
 {
 	int count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		count++; /* is it this simple? */
 		h = h->next;
 	}
 	return (count);
-}
-
-int main(void)
-{
-    list_t *head;
-    list_t *new;
-    list_t hello = {"World", 5, NULL};
-    size_t n;
-
-    head = &hello;
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->str = strdup("Hello");
-    new->len = 5;
-    new->next = head;
-    head = new;
-    n = list_len(head);
-    printf("-> %lu elements\n", n);
-    free(new->str);
-    free(new);
-    return (0);
 }
