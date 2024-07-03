b017bc00c689212c1318c79647b77eaae3e441fd
TheSnewby <stephen.j.newby@gmail.com>
2024-07-03 08:07:56 -0500

    more_singly

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "list.h"
+
+/**
+ * print_listint - prints all elements of a listint_t list
+ * @h: head of singly linked list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	listint_t temp = h;
+	size_t count = 0;
+
+	while (temp != NULL)
+	{
+		printf("%u\n", temp->n);
+		temp = temp->next;
+		count++;
+	}
+
+	free(temp);
+	return (count);
+}
