e805791e832b401df399e7a275ac4ec5612f81bc
TheSnewby <stephen.j.newby@gmail.com>
2024-07-03 10:07:49 -0500

    6-pop

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.c"
+
+/**
+ * deletes the head node of a listint_t list
+ * @head: head of linked list
+ *
+ * Return: head node's data, or 0 if empty
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp = NULL;
+	int n;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	n = *head->n;
+	temp = *head;
+	(*head) = *head->next;
+	free(temp);
+
+	return (n);
+}
