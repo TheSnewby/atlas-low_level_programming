ab4a7227ef429dd57654a157c6630319a1819b4e
TheSnewby <stephen.j.newby@gmail.com>
2024-07-03 08:48:44 -0500

    changed else while to temp->next

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -20,7 +20,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		*head = new_node;
 	else
 	{
-		while (temp != NULL)
+		while (temp->next != NULL)
 			temp = temp->next;
 		temp->next = new_node;
 	}
