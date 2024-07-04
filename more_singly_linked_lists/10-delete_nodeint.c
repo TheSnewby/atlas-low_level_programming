d16b0dbd81d2fdf40dda2012ebae2bd68ae19b71
TheSnewby <stephen.j.newby@gmail.com>
2024-07-04 10:01:38 -0500

    & -> &&

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -31,7 +31,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		count++;
 	}
 
-	if (temp == NULL & count < index - 1)
+	if (temp == NULL && count < index - 1)
 		return (-1);
 
 	before->next = temp -> next;
