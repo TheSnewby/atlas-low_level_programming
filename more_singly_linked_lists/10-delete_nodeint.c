a5a85eab7f3fa2e0a9483423be6d28453227bcba
TheSnewby <stephen.j.newby@gmail.com>
2024-07-04 10:22:07 -0500

    reintroduced *head == NULL

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp;
 	listint_t *before;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 	temp = *head;
 
