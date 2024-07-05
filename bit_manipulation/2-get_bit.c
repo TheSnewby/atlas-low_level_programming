d9065b1cb72c95e4ae4dd876f089ea178445b033
TheSnewby <stephen.j.newby@gmail.com>
2024-07-04 21:17:06 -0500

    changed almost everything to handle different cases

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,53 @@
+#include "main.h"
+/**
+ * _pow - recursively returns x ^ y
+ * @x: base int
+ * @y: power int
+ *
+ * Return: int
+*/
+
+int _pow(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	y--;
+	return (x * (_pow(x, y)));
+}
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: unsigned long value
+ * @index: target bit index in n
+ *
+ * Return: value at bit or -1 if error 
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int i;
+	char found = 'n';
+	int found_index;
+
+	if (n == 0)
+		return (0);
+	for (i = 63; i >= 0; i--)
+	{
+		if (n & (1UL << i))
+		{
+			found = 'y';
+			found_index = i;
+			break;
+		}
+	}
+	if (index > found_index)
+		return (-1);
+	else if (found == 'y' && n & (1UL << index))
+		return (1);
+	else
+		return (0);
+}
