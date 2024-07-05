b7cb03ac8fbc48cf6383402c48855b6aacb06d87
TheSnewby <stephen.j.newby@gmail.com>
2024-07-04 23:37:40 -0500

    betty

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -32,18 +32,3 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	}
 	return (sum);
 }
-
-int main(void)
-{
-    unsigned int n;
-
-    n = flip_bits(1024, 1);
-    printf("%u\n", n);
-    n = flip_bits(402, 98);
-    printf("%u\n", n);
-    n = flip_bits(1024, 3);
-    printf("%u\n", n);
-    n = flip_bits(1024, 1025);
-    printf("%u\n", n);
-    return (0);
-}
