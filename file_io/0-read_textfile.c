0f4ca15d925477ddea3c8236fa483e37233feca8
TheSnewby <stephen.j.newby@gmail.com>
2024-07-05 16:51:12 -0500

    0-read & file_io

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of file
+ * @letters: number of letters to read and print. letters or chars?
+ *
+ * Return: num of letters read & print, 0 if cannot open or read,
+ * 0 if filename is null, 0 if write fails or writes a different
+ * than expected amount
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int i;
+	int fd;
+	char buf[letters];
+	int read_return;
+	int write_return;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	read_return = read(fd, buf, letters);
+	if (read_return != letters)
+		return (0);
+	write_return = write(1, buf, letters);
+	if (write_return != letters)
+		return (0);
+	close(fd);
+	return (letters);
+}
