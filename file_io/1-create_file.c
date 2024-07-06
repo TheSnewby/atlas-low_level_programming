7359d246869e558bf6d79cfc856739fe60f2c1c3
TheSnewby <stephen.j.newby@gmail.com>
2024-07-06 11:31:24 -0500

    removed main

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -40,16 +40,3 @@ int create_file(const char *filename, char *text_content)
 	close(fd);
 	return (1);
 }
-int main(int ac, char **av)
-{
-    int res;
-
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = create_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
-}
