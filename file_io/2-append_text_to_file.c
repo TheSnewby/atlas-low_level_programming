650c5172e2d0c747f5e7dc64e74f00dccc702fb5
TheSnewby <stephen.j.newby@gmail.com>
2024-07-06 12:58:34 -0500

    initialized text_content_size to 0

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	int write_return;
-	int text_content_size;
+	int text_content_size = 0;
 
 	if (filename == NULL)
 		return (-1);
