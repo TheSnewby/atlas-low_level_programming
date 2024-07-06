59dbba28691441bfcd989ed1456339ab7785e1d6
TheSnewby <stephen.j.newby@gmail.com>
2024-07-06 11:31:02 -0500

    changed open to include another permission and it's apparently a variadic?!

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 		text_content_size++;
 	}
 
-	fd = open(filename, O_CREAT | O_WRONLY);
+	fd = open(filename, O_RDWR | O_CREAT, 0600);
 	if (fd < 0)
 		return (-1);
 	write_return = write(fd, text_content, text_content_size);
@@ -39,4 +39,17 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	close(fd);
 	return (1);
+}
+int main(int ac, char **av)
+{
+    int res;
+
+    if (ac != 3)
+    {
+        dprintf(2, "Usage: %s filename text\n", av[0]);
+        exit(1);
+    }
+    res = create_file(av[1], av[2]);
+    printf("-> %i)\n", res);
+    return (0);
 }
