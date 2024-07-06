cf108e5f8a4a8f30b8315052cf1d5327bde516ff
TheSnewby <stephen.j.newby@gmail.com>
2024-07-06 11:17:52 -0500

    changed unsigned to signed

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int write_return;
-	unsigned int text_content_size = 0;
+	int text_content_size = 0;
 
 	if (filename == NULL)
 		return (-1);
