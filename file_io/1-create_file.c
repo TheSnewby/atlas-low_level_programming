4d82a580d0c60b9034c08fad5616fac37e608518
TheSnewby <stephen.j.newby@gmail.com>
2024-07-06 11:38:34 -0500

    added O_TRUNC flag

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 		text_content_size++;
 	}
 
-	fd = open(filename, O_RDWR | O_CREAT, 0600);
+	fd = open(filename, O_RDWR | O_CREAT  | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
 	write_return = write(fd, text_content, text_content_size);
