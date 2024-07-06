857e96b17d6109597c011a8fcf03509536f0d246
TheSnewby <stephen.j.newby@gmail.com>
2024-07-06 11:17:11 -0500

    1-create

---
+++ file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file and populates it with a string
+ * @filename: name of file
+ * @text_content: string to initially populate file
+ *
+ * Return: 1 on success, -1 on failure(file cannot be created,
+ * cannot be written, write fails, etc), -1 filename being NULL
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int write_return;
+	unsigned int text_content_size = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_CREAT);
+		if (fd < 0)
+			return (-1);
+		return (1);
+	}
+
+	while (text_content[text_content_size])
+	{
+		text_content_size++;
+	}
+
+	fd = open(filename, O_CREAT | O_WRONLY);
+	if (fd < 0)
+		return (-1);
+	write_return = write(fd, text_content, text_content_size);
+	if (write_return != text_content_size)
+		return (-1);
+	close(fd);
+	return (1);
+}
