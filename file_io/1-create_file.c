ef543a7e779f20985a57bd301ad71d3c930ae7b7
TheSnewby <stephen.j.newby@gmail.com>
2024-07-06 12:29:34 -0500

    added mode 0600 to text_content is NULL case

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		fd = open(filename, O_CREAT);
+		fd = open(filename, O_CREAT, 0600);
 		if (fd < 0)
 			return (-1);
 		return (1);
