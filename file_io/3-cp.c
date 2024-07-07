81700ce131cefcabc876bfe1334a109a6f9058c8
TheSnewby <stephen.j.newby@gmail.com>
2024-07-07 00:05:39 -0500

    commented much code and removed test files from github

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,7 +13,7 @@
 void cp_exit(int fd_from, int fd_to, int error_no, char *name)
 {
 	int close_return;
-
+	/* prints the error messages and exits program if closing FDs is unecessary */
 	if (error_no == 97)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -27,7 +27,7 @@ void cp_exit(int fd_from, int fd_to, int error_no, char *name)
 	}
 	else if (error_no == 99)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", name);
-
+	/* attempts to close open FDs */
 	if (fd_from >= 0)
 	{
 		close_return = (fd_from);
@@ -48,8 +48,7 @@ void cp_exit(int fd_from, int fd_to, int error_no, char *name)
 			exit(100);
 		}
 	}
-
-	if (error_no == 98)
+	if (error_no == 98) /* exits program according to error number */
 		exit(98);
 	else if (error_no == 99)
 		exit(99);
@@ -69,20 +68,22 @@ void cp_exit(int fd_from, int fd_to, int error_no, char *name)
 
 int main(int argc, char *argv[])
 {
-	int fd_from;
-	int fd_to;
+	int fd_from; /* fd for file_from */
+	int fd_to; /* fd for file_to */
 	int read_return = 1; /* non-zero initial value */
-	int write_return;
-	char *file_from;
-	char *file_to;
-	char buf[1024];
+	int write_return; /* records return value of write_return */
+	char *file_from; /* filename of file_from */
+	char *file_to; /* filename of file_to */
+	char buf[1024]; /* buffer */
 
-	if (argc != 3)
+	if (argc != 3) /* incorrect number of args */
 		cp_exit(0, 0, 97, NULL);
 
+	/* assigns names from passed arguments */
 	file_from = argv[1];
 	file_to = argv[2];
 
+	/* opens files and assigns an fd. error if failed to open */
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from < 0)
 		cp_exit(fd_from, 0, 98, file_from);
@@ -90,6 +91,7 @@ int main(int argc, char *argv[])
 	if (fd_to < 0)
 		cp_exit(fd_from, fd_to, 99, file_to);
 
+	/* reads from fd_from until EOF (read_return == 0), exits if r/w error */
 	while (read_return)
 	{
 		read_return = read(fd_from, buf, 1024);
@@ -101,7 +103,8 @@ int main(int argc, char *argv[])
 			cp_exit(fd_from, fd_to, 99, file_to);
 	}
 
+	/* closes FDs */
 	cp_exit(fd_from, fd_to, 0, NULL);
 
-	return (0);
+	return (0); /* big success */
 	}
