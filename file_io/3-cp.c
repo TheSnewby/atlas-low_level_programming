09539a1fbeb275075a2b958eaebf78764360f3e7
TheSnewby <stephen.j.newby@gmail.com>
2024-07-06 22:58:18 -0500

    refactored for the 40 line function limit

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,18 +1,28 @@
 #include "main.h"
 
-int _exit(int fd_from, int fd_to, int error_message)
+/**
+ * cp_exit - handles errors and closes fds
+ * @fd_from: source fd
+ * @fd_to: target fd
+ * @error_no: error number if applicable
+ * @name: name of file if applicable
+ *
+ * Return: void
+ */
+
+void cp_exit(int fd_from, int fd_to, int error_no, char *name)
 {
 	int close_return;
 
-	/* practice with switches? */
-	if (error_message == 97)
-		dprint ...
-	else if (error_message == 98)
-		dprint ...
-	else if (error_message == 99)
-		dprint ...
-	else
-		dprint ...
+	if (error_no == 97)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	else if (error_no == 98)
+		dprintf(2, "Error: Can't read from file %s\n", name);
+	else if (error_no == 99)
+		dprintf(2, "Error: Can't write to %s\n", name);
 
 	close_return = (fd_from);
 	if (close_return < 0)
@@ -28,6 +38,11 @@ int _exit(int fd_from, int fd_to, int error_message)
 		dprintf(2, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
+
+	if (error_no == 98)
+		exit(98);
+	else if (error_no == 99)
+		exit(99);
 }
 
 /**
@@ -46,7 +61,7 @@ int main(int argc, char *argv[])
 {
 	int fd_from;
 	int fd_to;
-	int read_return = 1;
+	int read_return = 1; /* non-zero initial value */
 	int write_return;
 	char *file_from;
 	char *file_to;
@@ -54,62 +69,30 @@ int main(int argc, char *argv[])
 	char buf[1024];
 
 	if (argc != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		cp_exit(0, 0, 97, NULL);
 
 	file_from = argv[1];
 	file_to = argv[2];
 
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from < 0)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
+		cp_exit(fd_from, fd_to, 98, file_from);
 	fd_to = open(file_to, O_WRONLY | O_TRUNC | O_CREAT | O_APPEND, 0700);
 	if (fd_to < 0)
-	{
-		close(fd_from);
-		dprintf(2, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
+		cp_exit(fd_from, fd_to, 99, file_to);
 
 	while (read_return)
 	{
 		read_return = read(fd_from, buf, 1024);
 		if (read_return < 0)
-		{
-			close(fd_from);
-			close(fd_to);
-			dprintf(2, "Error: Can't read from file %s\n", file_from);
-			exit(98);
-		}
+			cp_exit(fd_from, fd_to, 98, file_from);
+
 		write_return = write(fd_to, buf, read_return);
 		if (write_return < 0 || write_return != read_return)
-		{
-			close(fd_from);
-			close(fd_to);
-			dprintf(2, "Error: Can't write to %s\n", file_to);
-			exit(99);
-		}
+			cp_exit(fd_from, fd_to, 99, file_to);
 	}
 
-	close_return = (fd_from);
-	if (close_return < 0)
-	{
-		close(fd_to);
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	close_return = (fd_to);
-	if (close_return < 0)
-	{
-		close(fd_to);
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
+	cp_exit(fd_from, fd_to, 0, NULL);
 
 	return (0);
 	}
--- file_io/main.h
+++ file_io/main.h
@@ -8,5 +8,6 @@
 ssize_t read_textfile(const char *filename, size_t letter);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void cp_exit(int, int, int, char *);
 
 #endif
