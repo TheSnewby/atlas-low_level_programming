ae3db9ac1913424e33e69f561f1bf3cb62972cb2
TheSnewby <stephen.j.newby@gmail.com>
2024-07-07 17:39:43 -0500

    updates main.h

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -9,5 +9,6 @@ int get_bit(unsigned long int, unsigned int);
 int set_bit(unsigned long int *, unsigned int);
 int clear_bit(unsigned long int *, unsigned int);
 unsigned int flip_bits(unsigned long int, unsigned long int);
+int get_endianness(void);
 
 #endif
