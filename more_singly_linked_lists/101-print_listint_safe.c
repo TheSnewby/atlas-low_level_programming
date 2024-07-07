50faaa60da1b0430ac0bd005756130a5ae9e7887
TheSnewby <stephen.j.newby@gmail.com>
2024-07-07 14:54:57 -0500

    101

---
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a listint_t linked list including node address
+ * @head: head of linked list
+ *
+ * Return: count of nodes
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	listint_t *temp = head;
+	size_t count = 0;
+
+	if (head == NULL)
+		exit(98);
+	while (head != NULL)
+	{
+		count++;
+		printf("%p %d\n", &head, head->n);
+		head = head->next;
+	}
+	return (count);
+}
