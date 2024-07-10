8e09bb8948709394e6d0e90dc2c3ca00fd000434
TheSnewby <stephen.j.newby@gmail.com>
2024-07-10 10:53:45 -0500

    forgot a couple bits

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,12 +12,18 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL || head == NULL)
+	{
+		free(new_node);
 		return (NULL);
+	}
+
 	if (*head != NULL)
 	{
 		(*head)->prev = new_node;
+		new_node->next = *head;
 	}
-	new_node->next = *head;
+	else
+		new_node->next = NULL;
 	new_node->prev = NULL;
 	new_node->n = n;
 
