457f67b9511215a2d59121ac45a0d4f7f72b5c37
TheSnewby <stephen.j.newby@gmail.com>
2024-07-10 10:51:22 -0500

    case for empty list

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -11,11 +11,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 
-	if (new_node == NULL)
+	if (new_node == NULL || head == NULL)
 		return (NULL);
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
 	new_node->next = *head;
 	new_node->prev = NULL;
-	(*head)->prev = new_node;
 	new_node->n = n;
 
 	return (new_node);
