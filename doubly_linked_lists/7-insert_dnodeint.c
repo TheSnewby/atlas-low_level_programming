c603667e93237b9c81bd0d4f50da2375660137eb
TheSnewby <stephen.j.newby@gmail.com>
2024-07-10 21:58:20 -0500

    changed idx = 0 logic

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -22,10 +22,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new_node->next = *h;
 		new_node->prev = NULL;
 		if (*h != NULL)
+		{
+			new_node->next = *h;
 			(*h)->prev = new_node;
+		}
+		else
+			new_node->next = NULL;
 		return (new_node);
 	}
 
---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - deltes the node at an index of a dlinked list
+ * @head: pointer to head of list
+ * @index: index to be deleted
+ *
+ * Return: 1 if success, -1 otherwise
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	
+}
