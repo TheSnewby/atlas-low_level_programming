6c05a1c0448a00eea441ccce335243cfeed4829a
TheSnewby <stephen.j.newby@gmail.com>
2024-07-11 10:57:01 -0500

    added free in new_ht->array null check, and moved the initialization of array past the calloc

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -12,15 +12,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_ht = NULL;
 	unsigned long int i;
 
-	new_ht->array = NULL;
 	new_ht = (hash_table_t *)malloc(sizeof(hash_table_t));
+	new_ht->array = NULL;
 	if (size == 0 || new_ht == NULL)
 		return (NULL);
 
 	new_ht->size = size;
 	new_ht->array = (hash_node_t **)calloc(new_ht->size, sizeof(hash_node_t *));
 	if (new_ht->array == NULL)
+	{
+		free(new_ht);
 		return (NULL);
+	}
 
 	for (i = 0; i < new_ht->size; i++)
 	{
