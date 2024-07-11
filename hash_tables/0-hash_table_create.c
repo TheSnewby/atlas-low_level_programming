ceb850887ecc9a59c809949f56f62645313c3340
TheSnewby <stephen.j.newby@gmail.com>
2024-07-11 09:47:58 -0500

    initialized the array

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,11 +9,23 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_ht = (hash_table_t *)malloc(sizeof(hash_table_t));
+	hash_table_t *new_ht = NULL;
+	int i;
 
+	new_ht->array = NULL;
+	new_ht = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (size == 0 || new_ht == NULL)
 		return (NULL);
 
 	new_ht->size = size;
+	new_ht->array = (hash_node_t **)calloc(new_ht->size, sizeof(hash_node_t *));
+	if (new_ht->array == NULL)
+		return (NULL);
+
+	for (i = 0; i < new_ht->size; i++)
+	{
+		new_ht->array[i] = NULL;
+	}
+
 	return (new_ht);
 }
