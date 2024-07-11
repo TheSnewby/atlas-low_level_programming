1986c16ddbfe569efbd1209ebdaf4d1e3454f814
TheSnewby <stephen.j.newby@gmail.com>
2024-07-11 11:29:29 -0500

    added logic if *head is null or not

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -17,8 +17,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (index == 0)
 	{
-		*head = (*head)->next;
-		(*head)->prev = NULL;
+		if (*head != NULL)
+		{
+			*head = (*head)->next;
+			(*head)->prev = NULL;
+		}
 		free(temp);
 		return (1);
 	}
