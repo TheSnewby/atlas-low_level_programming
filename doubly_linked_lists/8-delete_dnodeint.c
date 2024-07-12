3cbd8876b5361bcd95f5a55249024e7583b256fd
TheSnewby <stephen.j.newby@gmail.com>
2024-07-11 23:30:56 -0500

    added a temp->next == null condition

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -35,8 +35,15 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (temp == NULL)
 		return (-1);
 
-	temp->prev->next = temp->next;
-	temp->next->prev = temp->prev;
+	if (temp->next != NULL)
+	{
+		temp->prev->next = temp->next;
+		temp->next->prev = temp->prev;
+	}
+	else
+	{
+		temp->prev->next = NULL;
+	}
 	free(temp);
 	return (1);
 }
