cb187ff2b7e3a1ab7ea0c7069fcaad30dfac8de9
TheSnewby <stephen.j.newby@gmail.com>
2024-07-12 11:13:45 -0500

    changed signage of passed key

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL)
 		return (0);
-	index = hash_djb2(key) % ht->size;
+	index = hash_djb2((const unsigned char *)key) % ht->size;
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
