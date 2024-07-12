b8fc01aca78eb7959dcb8419c9849a3c93ee079a
TheSnewby <stephen.j.newby@gmail.com>
2024-07-12 17:37:28 -0500

    changed comparisons of string pointers to the actual strings

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -33,7 +33,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = new_node;
 	else /* if collision */
 	{
-		if (key != ht->array[index]->key)
+		if (strcmp(key, ht->array[index]->key))
 		{
 			new_node->next = ht->array[index];
 			ht->array[index] = new_node;
