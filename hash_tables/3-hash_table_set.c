8074aee6720ede72ee2b05d9ffeb6e826652dc49
TheSnewby <stephen.j.newby@gmail.com>
2024-07-12 11:23:16 -0500

    duplicated string to bypass const block

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -21,9 +21,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-	new_node->key = key;
+	new_node->key = strdup(key); /* duplicated to bypass const */
 	new_node->next = NULL;
-	new_node->value = value;
+	new_node->value = strdup(value);
+
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		return (0);
+	}
 
 	if (ht->array[index] == NULL)
 		ht->array[index] = new_node;
