f048229397489596097bcc87bcdc62a34ba560fd
TheSnewby <stephen.j.newby@gmail.com>
2024-07-13 10:35:53 -0500

    4-hashget

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table
+ * @key: key for value
+ *
+ * Return: value associated with key, NULL if not found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *temp = NULL;
+	unsigned long int i;
+
+	if (key == NULL)
+		return (NULL);
+	i = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[i];
+
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key))
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL);
+}
