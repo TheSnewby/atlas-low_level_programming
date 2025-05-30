1c91a0fdc4e5ca8e8e35fc4f815033515b93c8e5
TheSnewby <stephen.j.newby@gmail.com>
2025-05-30 12:26:14 -0500

    disabled main

--- hash_tables/100-sorted_hash_table.c
+++ hash_tables/100-sorted_hash_table.c
@@ -278,28 +278,28 @@ void shash_table_print_legacy(const shash_table_t *ht)
 }
 */
 
-int main(void)
-{
-    shash_table_t *ht;
+// int main(void)
+// {
+//     shash_table_t *ht;
 
-    ht = shash_table_create(1024);
-    shash_table_set(ht, "Holberton", "is cool");
-    shash_table_print(ht);
-    shash_table_set(ht, "Holberton", "is soooo cooool");
-    shash_table_print(ht);
-    shash_table_set(ht, "Holberton", "is awesome");
-    shash_table_print(ht);
-    shash_table_set(ht, "b", "3");
-    shash_table_print(ht);
-    shash_table_set(ht, "z", "4");
-    shash_table_print(ht);
-    shash_table_set(ht, "n", "5");
-    shash_table_print(ht);
-    shash_table_set(ht, "a", "6");
-    shash_table_print(ht);
-    shash_table_set(ht, "m", "7");
-    shash_table_print(ht);
-    shash_table_print_rev(ht);
-    shash_table_delete(ht);
-    return (EXIT_SUCCESS);
-}
+//     ht = shash_table_create(1024);
+//     shash_table_set(ht, "Holberton", "is cool");
+//     shash_table_print(ht);
+//     shash_table_set(ht, "Holberton", "is soooo cooool");
+//     shash_table_print(ht);
+//     shash_table_set(ht, "Holberton", "is awesome");
+//     shash_table_print(ht);
+//     shash_table_set(ht, "b", "3");
+//     shash_table_print(ht);
+//     shash_table_set(ht, "z", "4");
+//     shash_table_print(ht);
+//     shash_table_set(ht, "n", "5");
+//     shash_table_print(ht);
+//     shash_table_set(ht, "a", "6");
+//     shash_table_print(ht);
+//     shash_table_set(ht, "m", "7");
+//     shash_table_print(ht);
+//     shash_table_print_rev(ht);
+//     shash_table_delete(ht);
+//     return (EXIT_SUCCESS);
+// }
